8bc1ce72f8b94d1875e702ef99ebd187f761ccde
Marcos Calixto <marcos.calixto@huntercar.app>
2021-12-07 08:58:22 -0300

    recuperando tarefas e status do db

--- nova_tarefa.php
+++ nova_tarefa.php
@@ -19,6 +19,13 @@
 			</div>
 		</nav>
 
+		<? if (isset($_GET['inclusao']) && $_GET['inclusao'] == 1) { ?>
+		<div class="bg-success pt-2 text-white d-flex justify-content-center">
+			Tarefa inserida com sucesso!
+		</div>
+
+		<? } ?>
+
 		<div class="container app">
 			<div class="row">
 				<div class="col-md-3 menu">
@@ -36,7 +43,7 @@
 								<h4>Nova tarefa</h4>
 								<hr />
 
-								<form method="POST" action="tarefa_controller.php">
+								<form method="POST" action="tarefa_controller.php?acao=inserir">
 									<div class="form-group">
 										<label>Descrição da tarefa:</label>
 										<input name="tarefa" type="text" class="form-control" placeholder="Exemplo: Lavar o carro">
