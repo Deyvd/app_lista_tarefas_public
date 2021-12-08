97f9d278e4509f3fb0afef90b0523e959d872fc8
Marcos Calixto <marcos.calixto@huntercar.app>
2021-12-08 19:54:27 -0300

    implementação do metodo atualizar

--- todas_tarefas.php
+++ todas_tarefas.php
@@ -14,6 +14,53 @@
 		<link rel="stylesheet" href="css/estilo.css">
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+	
+		<script>
+			function editar(id, descricaoTarefa){
+				// alert('estamos aqui');
+
+				console.log(descricaoTarefa)
+
+				let form = document.createElement('form')
+				form.action = "tarefa_controller.php?acao=atualizar"
+				form.method = 'post'
+				form.className = 'row'
+
+				let inputTarefa = document.createElement('input')
+				inputTarefa.type = 'text'
+				inputTarefa.name = 'tarefa'
+				inputTarefa.className = 'form-control col-9'
+				inputTarefa.value = descricaoTarefa
+				
+				let inputId = document.createElement('input')
+				inputId.type = 'hidden'
+				inputId.name = 'id'
+				inputId.value = id
+
+
+				let button = document.createElement('button')
+				button.type = 'submit'
+				button.className = 'btn btn-info col-3'
+				button.innerHTML = 'Atualizar'
+
+
+				form.appendChild(inputTarefa)
+				form.appendChild(inputId)
+				form.appendChild(button)
+
+				// console.log(form)
+
+				let tarefa = document.getElementById('tarefa_'+id)
+
+				tarefa.innerHTML = ''
+
+				tarefa.insertBefore(form, tarefa[0])
+
+				
+
+			}
+		</script>
+	
 	</head>
 
 	<body>
@@ -46,10 +93,12 @@
 								<? foreach($tarefas as $tarefa) {   ?>	
 
 								<div class="row mb-3 d-flex align-items-center tarefa">
-									<div class="col-sm-9"><?= $tarefa->tarefa   ?> (<?= $tarefa->status   ?>) </div>
+									<div class="col-sm-9" id="tarefa_<?= $tarefa->id   ?>">
+										<?= $tarefa->tarefa   ?> (<?= $tarefa->status   ?>) 
+									</div>
 									<div class="col-sm-3 mt-2 d-flex justify-content-between">
 										<i class="fas fa-trash-alt fa-lg text-danger"></i>
-										<i class="fas fa-edit fa-lg text-info"></i>
+										<i class="fas fa-edit fa-lg text-info" onclick="editar(<?= $tarefa->id ?>, '<?= $tarefa->tarefa ?>' )"></i>
 										<i class="fas fa-check-square fa-lg text-success"></i>
 									</div>
 								</div>
